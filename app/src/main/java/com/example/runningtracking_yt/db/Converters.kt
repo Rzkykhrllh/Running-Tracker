09cd836a8de799be02a003a92988eb08260650b5
Rzkykhrllh <rizkykirigaya@gmail.com>
2021-03-02 21:14:27 +0800

    create Converters.kt and RunningDatabase.kt

---
+++ app/src/main/java/com/example/runningtracking_yt/db/Converters.kt
@@ -0,0 +1,31 @@
+package com.example.runningtracking_yt.db
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    /* Konversi bitmap ke bytearray
+    *  Database simpan dalam bentuk ByteArray*/
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+
+        /* Konvert bitmap ke byte array
+        * 1. format bitmap -> png
+        * 2. kualitas 100%
+        * 3. simpan kemana*/
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+
+        return outputStream.toByteArray()
+    }
+
+    /* Konversi bitmap ke bytearray
+    *  Database simpan dalam bentuk ByteArray*/
+    @TypeConverter
+    fun toBitmap(bytes: ByteArray): Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+
+}
